01476c450fe558aef1baed9e07271b0a2c5fac5d
Pavel Saltykov <pavsaltykov@mail.ru>
2022-10-26 22:35:18 +0300

    Add many measures

--- src/app/api.py
+++ src/app/api.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from src.app import measure_calculator
 from src.app.conll_converter import ConllConverter
+from src.app.measure_calculator import SentenceMeasure
 from src.app.models.request import SentenceMeasuresRequest
 from src.app.models.response import SentenceMeasuresResponse, MeasureResult
 
-app = FastAPI(title="text-complexity-service")
+app = FastAPI(title="text-complexity-service", default_response_class=ORJSONResponse)
 
 conll_converters = ConllConverter.all_language_converters()
 
@@ -24,12 +26,14 @@ async def sentences_measures(body: list[SentenceMeasuresRequest]):
             continue
 
         conll_sentences = conll_converters[item.language].text2conll_str(item.sentence).split("\n\n")
+        conll_sentences = list(filter(None, conll_sentences))
         if len(conll_sentences) != 1:
-            response.append(SentenceMeasuresResponse(id=item.id, error="Required only one sentence"))
+            response.append(SentenceMeasuresResponse(id=item.id, error="Required one sentence"))
             continue
 
         conll_sentence = conll_sentences[0]
-        calculated_measures = measure_calculator.calculate_for_sentence(conll_sentence)
-        calculated_measures = [MeasureResult(name=n, value=v) for n, v in calculated_measures]
+        calculated_measures = [measure_calculator.calculate_for_sentence(conll_sentence, m) for m in SentenceMeasure]
+        calculated_measures = [MeasureResult(name=res[0], value=res[1])
+                               for res in calculated_measures if res is not None]
         response.append(SentenceMeasuresResponse(id=item.id, measures=calculated_measures))
     return response
--- src/app/measure_calculator.py
+++ src/app/measure_calculator.py
@@ -2,33 +2,65 @@ import itertools
 from enum import Enum
 from typing import Tuple
 
-from textcomplexity import sentence
+from textcomplexity import surface, dependency
 from textcomplexity.utils import conllu
+from textcomplexity.utils.text import Text
 
 
 class MeasureType(Enum):
-    token = 1
+    text = 1
     graph = 2
 
 
-class Measure(Enum):
-    sentence_length_tokens = "sentence length (tokens)", MeasureType.token, sentence._sentence_length_tokens
+class SentenceMeasure(Enum):
+    text_length_tokens = "text length (tokens)", MeasureType.text, surface.text_length_tokens
+    text_length_characters = "text length (characters)", MeasureType.text, surface.text_length_characters
+    type_token_ratio = "type-token ratio", MeasureType.text, surface.type_token_ratio
+    guiraud_r = "Guiraud's R", MeasureType.text, surface.guiraud_r
+    herdan_c = "Herdan's C", MeasureType.text, surface.herdan_c
+    dugast_k = "Dugast's k", MeasureType.text, surface.dugast_k
+    maas_a2 = "Maas' a²", MeasureType.text, surface.maas_a2
+    dugast_u = "Dugast's U", MeasureType.text, surface.dugast_u
+    tuldava_ln = "Tuldava's LN", MeasureType.text, surface.tuldava_ln
+    brunet_w = "Brunet's W", MeasureType.text, surface.brunet_w
+    cttr = "CTTR", MeasureType.text, surface.cttr
+    summer_s = "Summer's S", MeasureType.text, surface.summer_s
+    sichel_s = "Sichel's S", MeasureType.text, surface.sichel_s
+    michea_m = "Michéa's M", MeasureType.text, surface.michea_m
+    honore_h = "Honoré's H", MeasureType.text, surface.honore_h
+    entropy = "entropy", MeasureType.text, surface.entropy
+    evenness = "evenness", MeasureType.text, surface.evenness
+    jarvis_evenness = "Jarvis's evenness", MeasureType.text, surface.jarvis_evenness
+    yule_k = "Yule's K", MeasureType.text, surface.yule_k
+    simpson_d = "Simpson's D", MeasureType.text, surface.simpson_d
+    herdan_vm = "Herdan's Vm", MeasureType.text, surface.herdan_vm
+    hdd = "HD-D", MeasureType.text, surface.hdd
+    average_token_length = "average token length", MeasureType.text, surface.average_token_length
+    orlov_z = "Orlov's Z", MeasureType.text, surface.orlov_z
+    mtld = "MTLD", MeasureType.text, surface.mtld
+
+    average_dependency_distance = ("average dependency distance", MeasureType.graph,
+                                   dependency._average_dependency_distance)
+    closeness_centrality = "closeness centrality", MeasureType.graph, dependency._closeness_centrality
+    outdegree_centralization = "outdegree centralization", MeasureType.graph, dependency._outdegree_centralization
+    closeness_centralization = "closeness centralization", MeasureType.graph, dependency._closeness_centralization
+    longest_shortest_path = "longest shortest path", MeasureType.graph, dependency._longest_shortest_path
+    dependents_per_word = "dependents per word", MeasureType.graph, dependency._dependents_per_word
 
 
-def calculate_for_sentence(conll_sentence: str) -> list[Tuple[str, float]]:
+def calculate_for_sentence(conll_sentence: str, measure: SentenceMeasure) -> Tuple[str, float] | None:
     conll_sentence = conll_sentence.split("\n")
     sentences, graphs = zip(*conllu.read_conllu_sentences(conll_sentence))
     if len(sentences) > 1:
         raise ValueError()
-    tokens = list(itertools.chain.from_iterable(sentences))
+    sentence_tokens = list(itertools.chain.from_iterable(sentences))
+    graph = graphs[0]
 
-    results = []
-    for m in Measure:
-        m_name, m_type, m_func = m.value
-        if m_type == MeasureType.token:
-            results.append((m_name, m_func(tokens)))
-        elif m_type == MeasureType.graph:
-            results.append((m_name, m_func(graphs)))
-        else:
-            continue
-    return results
+    m_name, m_type, m_func = measure.value
+    if m_type == MeasureType.text:
+        text = Text.from_tokens(sentence_tokens)
+        return m_name, m_func(text)
+    elif m_type == MeasureType.graph:
+        return m_name, m_func(graph)
+    else:
+        return None
